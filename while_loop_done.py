1855f71a9a760144edc950317f5c9fd531938ff8
AyobamiH <heryourbarme@live.com>
2020-08-22 02:35:07 +0100

    Option to input upto 5 users\n appended a dict item to list.

--- while_loop_done.py
+++ while_loop_done.py
@@ -17,149 +17,163 @@
 
 
 
-
+# Empty list to append dictionary items at the end of the program.
 employee_data = []
-users = 0
-while users < 5:
 
+# While loop flag.
+count = 0
+
+
+# Inform user of maximum data storage per session.
+print('You can store upto 5 employee datas per session.')
+
+# Ask user for input.
+users = input('Number of employee datas to store ?: ')
+users = int(users)
+
+# Check to make sure user is not exceeding limit.
+if users > 5:
+    pass
+else:
+    while count < users:
 
     #<<<<-----------User Employee ID------------>>>
-    num_ok = False
+        num_ok = False
 
-    while not num_ok:
-        num = input('Enter number:')
+        while not num_ok:
+            num = input('Enter number:')
 
 
-        # Check for bad characters in user input and terminate if  bad character is found.
-        # Check to see user input isn't blank.
-        if num:
-            
-
-            # Checking the user input to see if it is truly integer.
-            # Using try and  except statement to control error output.
-            try:
-
-                int(num)
-                num_ok = True
+            # Check for bad characters in user input and terminate if  bad character is found.
+            # Check to see user input isn't blank.
+            if num:
                 
-                # If length  is less than or equal to seven.
-                if len(num) <= 7:
+
+                # Checking the user input to see if it is truly integer.
+                # Using try and  except statement to control error output.
+                try:
+
+                    int(num)
                     num_ok = True
+                    
+                    # If length  is less than or equal to seven.
+                    if len(num) <= 7:
+                        num_ok = True
 
-                else:
-                    num_ok = False
-
-            except:
-                num_ok = False
-                
-        else:
-            num_ok = False
-
-    #<<<------------User Employee Name------------>>>
-
-    name_ok = False
-    
-    characters =  list('!"@#$%^&*()_=+,<\>/?;:[]{}\).')
-
-    while not name_ok:
-        
-
-        name = input('Enter name:')
-
-        if name.istitle():
-            name_ok = True
-            
-        else:
-            name_ok = False
-
-   #<<<------------User Employee Address------------>>>
-
-            
-    bad_chars = list("!\"'@$%^&*_=+<>?;:[]{}).")
-
-    # Flag = keeps while loop under control. 
-    address_ok = False   
-
-
-    while not address_ok:
-
-        # Get user input.
-        address = input('Enter postal address:  ')
-
-        # Check to make sure user input is not blank.
-        if address:
-
-            #Empty string to help wih the check along the way.
-            new_address = ''
-
-            # For loop to iterate through elements of user input
-            for element in address:
-                
-                # Validating address by check for numeric, alphabet and space(' ') character.
-                if ((element.isnumeric() or element.isalpha()) or element.isspace()):
-
-                    # If condition above is true concatenate the letter with  empty string from above.
-                    new_address += element
-
-                    # If element of address in the list of bad characters ask user for input.
-                    if element in bad_chars:
-                        address_ok = False
-                        
                     else:
-                        address_ok = True
-                        
-                        
-        else:
-            address_ok = True
+                        num_ok = False
 
-    #<<<------------User Employee Email------------>>>
-            
-    active = False
-    characters =  list('!"\'#$%^&*()=+,<>/?;:[]{}\)')
+                except:
+                    num_ok = False
+                    
+            else:
+                num_ok = False
 
+        #<<<------------User Employee Name------------>>>
 
-    while not active:
-        email = input('Enter email:')
-
+        name_ok = False
         
-        # Check for bad characters in user input and terminate if  bad character is found.
-        # Blank check
-        if email:
+        characters =  list('!"@#$%^&*()_=+,<\>/?;:[]{}\).')
 
-            #Empty string to help wih the check along the way.
-            new_email = ''
+        while not name_ok:
+            
 
-            # For loop to iterate through elements of user input.
-            for letter in email:
+            name = input('Enter name:')
+
+            if name.istitle():
+                name_ok = True
+                
+            else:
+                name_ok = False
+
+       #<<<------------User Employee Address------------>>>
+
+                
+        bad_chars = list("!\"'@$%^&*_=+<>?;:[]{}).")
+
+        # Flag = keeps while loop under control. 
+        address_ok = False   
 
 
-                # Check for numbers, alphabets, '@' symbol and '.'.
-                if (letter.isnumeric() or letter.isalpha()) or (letter == '@' or letter == '.'):
+        while not address_ok:
 
-                     # If condition above is true concatenate the letter with our empty string from above.
-                    new_email += letter
+            # Get user input.
+            address = input('Enter postal address:  ')
+
+            # Check to make sure user input is not blank.
+            if address:
+
+                #Empty string to help wih the check along the way.
+                new_address = ''
+
+                # For loop to iterate through elements of user input
+                for element in address:
+                    
+                    # Validating address by check for numeric, alphabet and space(' ') character.
+                    if ((element.isnumeric() or element.isalpha()) or element.isspace()):
+
+                        # If condition above is true concatenate the letter with  empty string from above.
+                        new_address += element
+
+                        # If element of address in the list of bad characters ask user for input.
+                        if element in bad_chars:
+                            address_ok = False
+                            
+                        else:
+                            address_ok = True
+                            
+                            
+            else:
+                address_ok = True
+
+        #<<<------------User Employee Email------------>>>
+                
+        active = False
+        characters =  list('!"\'#$%^&*()=+,<>/?;:[]{}\)')
 
 
-                    # Terminate the program if we have any bad character present in user input.
-                    if letter in characters:
+        while not active:
+            email = input('Enter email:')
+
+            
+            # Check for bad characters in user input and terminate if  bad character is found.
+            # Blank check
+            if email:
+
+                #Empty string to help wih the check along the way.
+                new_email = ''
+
+                # For loop to iterate through elements of user input.
+                for letter in email:
+
+
+                    # Check for numbers, alphabets, '@' symbol and '.'.
+                    if (letter.isnumeric() or letter.isalpha()) or (letter == '@' or letter == '.'):
+
+                         # If condition above is true concatenate the letter with our empty string from above.
+                        new_email += letter
+
+
+                        # Terminate the program if we have any bad character present in user input.
+                        if letter in characters:
+                            active = False
+                            
+                        else:
+                            active = True
+
+                    #  If user input does not have characters of a real email address.
+                    # Return to start of while loop and ask user to enter email address again
+                    else:
+                        
                         active = False
                         
-                    else:
-                        active = True
-
-                #  If user input does not have characters of a real email address.
-                # Return to start of while loop and ask user to enter email address again
-                else:
-                    
-                    active = False
-                    
-                if not active:
-                    
-                    email = input('Email was not entered properly\nPlease try again:')
+                    if not active:
+                        
+                        email = input('Email was not entered properly\nPlease try again:')
 
 
-
-    employee_data.append(f"'#{users}': 'ID num': {num},  'Name': {name}, 'Employee Address': {address}, 'Employee Email': {email}")
-    users += 1
+        # Appending dictionary object to list.
+        employee_data.append({'ID num': num,  'Name': name, 'Employee Address': address, 'Employee Email': email })
+        count = count + 1
 
 print(employee_data)
